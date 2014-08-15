b264ee26d575ba24b06150c9534f50b6ef1862ef
Vinicius <luiz.vinicius73@gmail.com>
2014-08-15 14:12:00 -0300

    init

---
+++ src/Vinicius73/Lavatar/LavatarServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Vinicius73\Lavatar;
+
+use Illuminate\Support\ServiceProvider;
+
+class LavatarServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('vinicius73/lavatar');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
