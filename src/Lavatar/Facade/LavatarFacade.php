75c40b4d7bd9fbe3bc34d8e4da05e97247e39358
Vinicius Reis <luiz.vinicius73@gmail.com>
2015-11-20 16:56:06 -0200

    fix alias name

--- src/Lavatar/Facade/LavatarFacade.php
+++ src/Lavatar/Facade/LavatarFacade.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Facade;
 
 class LavatarFacade extends Facade
 {
-   protected static function getFacadeAccessor() { return 'vinicius73.lavatar'; }
+   protected static function getFacadeAccessor() { return 'lavatar'; }
 }
